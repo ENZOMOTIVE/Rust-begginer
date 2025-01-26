8d18ae79e73da5fa8a61fa40a441483d08b1c7fd
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2025-01-26 17:04:58 +0530

    fibonacci.rs
    This teaches how Fibonacci series can be coded using rust

---
+++ basic programs/fibonacci.rs
@@ -0,0 +1,29 @@
+// fibonacci of a number it takes as input
+
+fn main(){
+    println!("{}", fib(10));
+}
+
+fn fib(num: i32) -> i32{
+    let mut first = 0;
+    let mut second = 1;
+    if num == 0 {
+        return first;
+    }
+    if num == 1 {
+        return second;
+    }
+    
+    for _ in 0..(num - 2) {
+        let temp = second;
+        second = second + first;
+        first = temp;
+    }
+    return second;
+}
+
+
+
+
+// * Note: Any variable we define in rust by default is a constant
+// mut means the thing which is not constant
